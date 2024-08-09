fb922fd3788fc538010d40b2eed1280fbc563734
vishal vishwakarma <94911571+vishal12123@users.noreply.github.com>
2024-08-09 08:41:28 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -71,7 +71,7 @@ function login() {
     <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
       <div className="flex items-center justify-center gap-2 text-white">
         <Image src="/whatsapp.gif" alt="Whatsapp" height={300} width={300} />
-        <span className="text-7xl">Whatsapp</span>
+        <span className="text-7xl">Contactcom</span>
       </div>
 
       <button
